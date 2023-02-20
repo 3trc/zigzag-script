1e67a6f39be96294edad9befc128180df7671075
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-20 18:40:15 +0800

    基本的ws

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import WebSocket from 'ws';
 
-console.log('你好，世界');
+async function main() {
+  console.log('你好，世界');
+  const ws = new WebSocket('ws://www.host.com/path');
+  ws.on('error', console.error);
+  ws.on('open', function open() {
+    ws.send('something');
+  });
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+}
+
+main();
