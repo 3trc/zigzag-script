8aabc02de940213d138e1f0a80e513bfe273f5e1
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-20 21:19:18 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import WebSocket from 'ws';
 
 async function main() {
   console.log('你好，世界');
-  const ws = new WebSocket('ws://www.host.com/path');
+  const ws = new WebSocket('wss://zigzag-exchange.herokuapp.com');
   ws.on('error', console.error);
   ws.on('open', function open() {
-    ws.send('something');
+    console.log('open');
+    // ws.send('something');
   });
   ws.on('message', function message(data) {
     console.log('received: %s', data);
