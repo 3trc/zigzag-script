f376c8724f56b0ab6b7911d6349abc29ab91d868
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-21 15:51:24 +0800

    报错

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const secret = require('../.secret.json');
 // http://127.0.0.1:7890
 const proxy = HttpsProxyAgent('socks5://127.0.0.1:7890');
 
-async function main() {
+async function zks() {
   const ethProvider = new ethers.providers.JsonRpcProvider(secret.rpcUrl);
   const ethWallet = new ethers.Wallet(secret.privateKey, ethProvider);
   // const ethWallet = ethers.Wallet.fromMnemonic(secret.phrase, `m/44'/60'/0'/0/${0}`).connect(ethProvider);
@@ -34,27 +34,51 @@ async function main() {
   console.log(order);
 }
 
-// async function main() {
-//   console.log('你好，世界');
-//   const ws = new WebSocket('wss://zigzag-exchange.herokuapp.com', {
-//     agent: proxy,
-//   });
-//   ws.on('error', console.error);
-//   ws.on('open', function open() {
-//     console.log('open');
-//     ws.send({ "op": "login", "args": [1002, "27334"] }.toString(), (err) => {
-//       console.log(err);
-//     });
-//   });
-//   ws.on('message', (json) => {
-//     try {
-//       // console.log(json.toString());
-//       const data = JSON.stringify(JSON.parse(json.toString()), null, 2) + ',\n';
-//       fs.appendFileSync('data.json', data, 'utf-8');
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   });
-// }
+async function main() {
+  const ethProvider = new ethers.providers.JsonRpcProvider(secret.rpcUrl);
+  const ethWallet = new ethers.Wallet(secret.privateKey, ethProvider);
+  const zksProvider = await zksync.getDefaultProvider('mainnet');
+  const zksWallet = await zksync.Wallet.fromEthSigner(ethWallet, zksProvider);
+  const accountId = (await zksWallet.getAccountId())?.toString() || '';
+  console.log('账户Id', accountId);
+  const order = await zksWallet.signOrder({
+    tokenSell: 'ZZ',
+    tokenBuy: 'USDC',
+    ratio: zksync.utils.tokenRatio({ ZZ: '1', USDC: '3.5' }),
+    amount: zksProvider.tokenSet.parseToken('ZZ', '1'),
+  });
+  const params = {
+    op: 'submitorder3',
+    args: [
+      1,
+      'ZZ-USDC',
+      order,
+    ],
+  };
+  console.log(params);
+  const ws = new WebSocket('wss://zigzag-exchange.herokuapp.com', { agent: proxy });
+  ws.on('error', console.error);
+  ws.on('open', () => {
+    console.log('open');
+    ws.send(JSON.stringify({
+      op: 'login',
+      args: [
+        1,
+        accountId,
+      ],
+    }), (err) => {
+      console.log(err);
+      ws.send(JSON.stringify(params), (err) => console.log);
+    });
+  });
+  ws.on('message', (json) => {
+    try {
+      const data = JSON.stringify(JSON.parse(json.toString()), null, 2) + ',\n';
+      fs.appendFileSync('data.json', data, 'utf-8');
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
 
 main();
