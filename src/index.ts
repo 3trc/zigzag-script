fa18ebb760a930baf8d22d11c62cdbcbdb7c2e56
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-21 15:27:12 +0800

    fix: 提交代码

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,20 @@ const secret = require('../.secret.json');
 const proxy = HttpsProxyAgent('socks5://127.0.0.1:7890');
 
 async function main() {
-  console.log('你好，世界');
   const ethProvider = new ethers.providers.JsonRpcProvider(secret.rpcUrl);
   const ethWallet = new ethers.Wallet(secret.privateKey, ethProvider);
   // const ethWallet = ethers.Wallet.fromMnemonic(secret.phrase, `m/44'/60'/0'/0/${0}`).connect(ethProvider);
   console.log(ethWallet.address);
   const zksProvider = await zksync.getDefaultProvider('mainnet');
   const zksWallet = await zksync.Wallet.fromEthSigner(ethWallet, zksProvider);
-  const balance = await zksWallet.getBalance('ZZ', 'verified');
-  console.log(ethers.utils.formatEther(balance));
-  const info = await zksWallet.getAccountState();
-  console.log(info);
+  // const balance = await zksWallet.getBalance('ZZ');
+  // console.log(ethers.utils.formatEther(balance));
+  // const info = await zksWallet.getAccountState();
+  // console.log(info);
   const order = await zksWallet.signOrder({
     tokenSell: 'ZZ',
     tokenBuy: 'USDC',
-    ratio: zksync.utils.tokenRatio({
-      ZZ: '1',
-      USDC: '3.5',
-    }),
+    ratio: zksync.utils.tokenRatio({ ZZ: '1', USDC: '3.5' }),
     amount: zksProvider.tokenSet.parseToken('ZZ', '1'),
   });
   console.log(order);
