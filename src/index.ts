ef8b496782714b69b773c6dd8c9ae9a3136c1007
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-21 12:42:42 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,12 @@ const proxy = HttpsProxyAgent('socks5://127.0.0.1:7890');
 async function main() {
   console.log('你好，世界');
   const ethProvider = new ethers.providers.JsonRpcProvider(secret.rpcUrl);
-  const ethWallet = ethers.Wallet.fromMnemonic(secret.phrase, `m/44'/60'/0'/0/${0}`).connect(ethProvider);
+  const ethWallet = new ethers.Wallet(secret.privateKey, ethProvider);
+  // const ethWallet = ethers.Wallet.fromMnemonic(secret.phrase, `m/44'/60'/0'/0/${0}`).connect(ethProvider);
+  console.log(ethWallet.address);
   const zksProvider = await zksync.getDefaultProvider('mainnet');
   const zksWallet = await zksync.Wallet.fromEthSigner(ethWallet, zksProvider);
-  const balance = await zksWallet.getBalance('ETH');
+  const balance = await zksWallet.getBalance('ZZ');
   console.log(ethers.utils.formatEther(balance));
 }
 
